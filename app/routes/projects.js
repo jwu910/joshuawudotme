f608462bc85f8110610ebabe0239134c8604e341
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-05-18 02:29:53 -0700

    Add project cards and set up for projects

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model: function() {
+    return [
+      {
+        description: "Interactive shell that that lets you checkout Git branches with fewer strokes.",
+        image: "https://raw.githubusercontent.com/jwu910/check-it-out/gh-pages/assets/images/logo.png",
+        source: "https://github.com/jwu910/check-it-out",
+        title: "Check It Out",
+        website: "http://checkit.club",
+      }
+    ]
+  }
 });
