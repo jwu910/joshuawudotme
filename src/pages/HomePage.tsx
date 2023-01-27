7d1bd079387f4b1d4a88e2a02de91ad5f34bb961
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-01-27 15:50:57 -0800

    feat: add zeebo, add nav bar

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,20 @@
+import { Box, Stack } from "@mui/material";
+// import profile from "../assets/profile.jpg";
+
+const HomePage = () => {
+  return (
+    <Box component="section">
+      <Stack sx={{ m: "auto" }}>
+        {/* <img
+          height="100"
+          width="100"
+          src={profile}
+          className="App-profile"
+          alt="profile"
+        /> */}
+      </Stack>
+    </Box>
+  );
+};
+
+export default HomePage;
