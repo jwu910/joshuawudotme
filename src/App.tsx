9aaf45d13281765f34b09663f1c3bef476be3fc8
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-01-27 15:50:57 -0800

    chore: nuke old code, install material

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+// import github from "./assets/github.png";
+// import linkedin from "./assets/linkedin.png";
+import profile from "./assets/profile.jpg";
+import logo from "./assets/wu_logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <section className="App-content">
+        <img src={profile} className="App-profile" alt="profile" />
+      </section>
     </div>
   );
 }
