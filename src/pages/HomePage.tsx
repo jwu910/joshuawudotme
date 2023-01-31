faf8f5f83d0612793b13a41fe9d617bbfa030d35
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-01-31 13:31:32 -0800

    chore: fix theme colors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography, useTheme } from "@mui/material";
 import profile from "../assets/profile.jpg";
 
 const HomePage = () => {
+  const theme = useTheme();
+
   return (
     <Box component="section">
       <Stack sx={{ m: "auto" }} spacing={3}>
@@ -9,7 +11,13 @@ const HomePage = () => {
           alt="profile photo"
           component="img"
           src={profile}
-          sx={{ height: 200, width: 200, borderRadius: "50%", m: "auto" }}
+          sx={{
+            height: 200,
+            width: 200,
+            borderRadius: "50%",
+            m: "auto",
+            boxShadow: `0 0 12px ${theme.palette.primary.main}`,
+          }}
         />
         <Typography variant="h1" sx={{ textAlign: "center" }}>
           Hello Nerds, I'm Josh
