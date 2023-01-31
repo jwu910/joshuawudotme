b74747bccac1f663eea6cd1cbbd67d7855cabe99
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-01-31 09:07:22 -0800

    chore: Add footer, logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import { Stack } from "@mui/material";
+import {
+  Container,
+  CssBaseline,
+  Stack,
+  useMediaQuery,
+} from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.scss";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
@@ -15,37 +20,55 @@ const HomePage = React.lazy(() => import("./pages/HomePage"));
 */
 
 function App() {
-  // TODO: create theme
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+          contrastThreshold: 3,
+        },
+      }),
+    [prefersDarkMode]
+  );
 
   return (
-    <Router>
-      <Stack
-        sx={{ m: "auto", height: "100vh" }}
-        spacing={2}
-        justifyContent="space-between"
-      >
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Suspense fallback={<CircularProgress />}>
-                <HomePage />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/projects"
-            element={
-              <Suspense fallback={<CircularProgress />}>
-                <ProjectsPage />
-              </Suspense>
-            }
-          />
-        </Routes>
-        <Footer />
-      </Stack>
-    </Router>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline enableColorScheme />
+        <Router>
+          <Navbar />
+          <Container maxWidth="md">
+            <Stack
+              sx={{ pt: 12, height: "calc(100vh - 56px)" }}
+              spacing={2}
+              justifyContent="space-between"
+            >
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <Suspense fallback={<CircularProgress />}>
+                      <HomePage />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="/projects"
+                  element={
+                    <Suspense fallback={<CircularProgress />}>
+                      <ProjectsPage />
+                    </Suspense>
+                  }
+                />
+              </Routes>
+            </Stack>
+            <Footer />
+          </Container>
+        </Router>
+      </ThemeProvider>
+    </>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,9 @@
-import { Box, Link, Stack } from "@mui/material";
-import githubLogo from "../assets/github.png";
-import linkedInLogo from "../assets/linkedin.png";
+import { Box, Link, Stack, useTheme } from "@mui/material";
 import logo from "../assets/wu_logo.png";
 // import { Link as RouterLink, matchPath, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const theme = useTheme();
   // const useRouteMatch = (patterns: readonly string[]) => {
   //   const { pathname } = useLocation();
 
@@ -23,7 +22,19 @@ const Navbar = () => {
   // const currentTab = routeMatch?.pattern?.path;
 
   return (
-    <Box component="header" sx={{ display: "flex", height: "auto", p: 2 }}>
+    <Box
+      component="header"
+      sx={{
+        display: "flex",
+        height: "auto",
+        p: 2,
+        left: 0,
+        top: 0,
+        position: "fixed",
+        width: "100vw",
+        backgroundColor: theme.palette.grey[600],
+      }}
+    >
       <img src={logo} height="40" alt="logo" />
 
       {/* <Tabs sx={{ ml: 2 }} value={currentTab}>
@@ -44,7 +55,7 @@ const Navbar = () => {
 
       <Stack
         direction="row"
-        sx={{ ml: "auto", verticalAlign: "middle" }}
+        sx={{ ml: "auto", verticalAlign: "middle", display: "flex" }}
         spacing={2}
       >
         <Link
@@ -53,8 +64,21 @@ const Navbar = () => {
           rel="noopener noreferrer"
           target="_blank"
           title="LinkedIn"
+          sx={{
+            height: "30px",
+            width: "30px",
+            m: 0,
+            display: "flex",
+            verticalAlign: "middle",
+          }}
         >
-          <img height="40" src={linkedInLogo} alt="LinkedIn company logo" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            fill={theme.palette.primary.main}
+          >
+            <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+          </svg>{" "}
         </Link>
 
         <Link
@@ -63,8 +87,21 @@ const Navbar = () => {
           rel="noopener noreferrer"
           target="_blank"
           title="Github"
+          sx={{
+            height: "30px",
+            width: "30px",
+            m: 0,
+            display: "flex",
+            verticalAlign: "middle",
+          }}
         >
-          <img height="40" src={githubLogo} alt="LinkedIn company logo" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 496 512"
+            fill={theme.palette.primary.main}
+          >
+            <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+          </svg>
         </Link>
       </Stack>
     </Box>
