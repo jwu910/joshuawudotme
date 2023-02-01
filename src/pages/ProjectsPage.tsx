fd7a0dfb3744318b577fa0aaf5eafd027b5d6519
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-02-01 10:38:30 -0800

    chore: add sus eyes to projects

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -2,7 +2,20 @@ const ProjectsPage = () => {
   // get projects from github api
   // filter projects with pages
   // list out projects
-  return <></>;
+  return (
+    <>
+      <div style={{ display: "flex", margin: "auto" }}>
+        <a href="https://emoji.gg/emoji/3578_sus">
+          <img
+            src="https://cdn3.emoji.gg/emojis/3578_sus.png"
+            width="64px"
+            height="48px"
+            alt="sus"
+          />
+        </a>
+      </div>
+    </>
+  );
 };
 
 export default ProjectsPage;
