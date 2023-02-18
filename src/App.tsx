1b7c10c0bfb32c7bdf9e455a6e30496b245b137f
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-02-18 13:53:02 -0800

    chore: remove react lazy dynamic import issue on netlify

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { Container, CssBaseline, Stack } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import JWThemeProvider from "./context/theme";
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
 
-const ProjectsPage = React.lazy(() => import("./pages/ProjectsPage"));
-const HomePage = React.lazy(() => import("./pages/HomePage"));
+// const ProjectsPage = React.lazy(() => import("./pages/ProjectsPage"));
+// const HomePage = React.lazy(() => import("./pages/HomePage"));
 // Requirements:
 /*
 - List of projects with github pages
