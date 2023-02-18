26cc51424895bf8f09654b44f79f9f340de4e954
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-02-17 23:32:39 -0800

    chore: add base component for widet

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,24 +1,25 @@
 import { Box, Stack, Typography, useTheme } from "@mui/material";
+import CalendlyWidget from "../components/CalendlyWidget";
 
 const ContactPage = () => {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    return (
-        <Box
-            component="section"
-            sx={{
-                display: "flex",
-                flexDirection: "column",
-                height: "100%",
-                justifyContent: "space-around",
-                p: 4,
-            }}
-        >
-            <Stack spacing={3}>
-               Test
-            </Stack>
-        </Box>
-    );
+  return (
+    <Box
+      component="section"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        justifyContent: "space-around",
+        p: 4,
+      }}
+    >
+      <Stack spacing={3}>
+        <CalendlyWidget />
+      </Stack>
+    </Box>
+  );
 };
 
 export default ContactPage;
