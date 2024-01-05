8e33e76d645427f6425cff46faba3c8fd7ef597f
Joshua Wu <Joshua.wu.0910@gmail.com>
2024-01-04 16:30:44 -0800

    chore: fix ts type for localstorage hook

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -1,17 +1,23 @@
 import React from "react";
 
-const useLocalStorage = (storageKey: string, fallbackState: any) => {
-  const [value, setValue] = React.useState(
-    JSON.parse(
-      localStorage.getItem(storageKey) ?? JSON.stringify(fallbackState),
-    ),
-  );
+const useLocalStorage = <T>(storageKey: string, fallbackState: T) => {
+  const [value, setValue] = React.useState<T>(() => {
+    const storedValue = localStorage.getItem(storageKey);
+    if (storedValue) {
+      try {
+        return JSON.parse(storedValue) as T;
+      } catch (error) {
+        console.error(`Error parsing stored value for key '${storageKey}':`, error);
+      }
+    }
+    return fallbackState;
+  });
 
   React.useEffect(() => {
     localStorage.setItem(storageKey, JSON.stringify(value));
   }, [value, storageKey]);
 
-  return [value, setValue];
+  return [value, setValue] as const;
 };
 
 export default useLocalStorage;
